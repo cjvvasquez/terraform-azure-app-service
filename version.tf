da93d55577cbbe93a708eeb7895acc7a33e95c17
Christian Vargas <cjvvasquez@gmail.com>
2025-10-14 23:51:52 -0500

    Beta 5.0  Project

--- version.tf
+++ version.tf
@@ -12,10 +12,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  client_id       = "0ad7047b-f80d-47bc-b8fb-08441854f8a5"
-  client_secret   = var.client_secret
-  tenant_id       = "d2e93b13-5a75-4431-ac08-eea488f9779b"
-  subscription_id = "21c24c5b-0b2f-4c10-a2fb-db612f726d74"
-
 }
